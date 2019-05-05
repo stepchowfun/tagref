0d57384d60e4d621ada47c20ab52de442e8ebec9
Stephan Boyer <stephan@stephanboyer.com>
2019-05-04 18:39:45 -0700

    Install v0.1.0 by default and use that version for CI

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@
   DESTINATION="${PREFIX:-/usr/local/bin}/tagref"
 
   # Which version to download
-  RELEASE="v${VERSION:-0.0.8}"
+  RELEASE="v${VERSION:-0.1.0}"
 
   # Determine which binary to download.
   FILENAME=''
