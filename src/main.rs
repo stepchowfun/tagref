f0943b7d681f1bd32044056bcff94b5c9cdbec8f
Stephan Boyer <stephan@stephanboyer.com>
2019-05-06 00:47:15 -0700

    Release v1.0.0

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ use std::{
 extern crate lazy_static;
 
 // The program version
-const VERSION: &str = "0.1.0";
+const VERSION: &str = "1.0.0";
 
 // Command-line option and subcommand names
 const CHECK_COMMAND: &str = "check";
