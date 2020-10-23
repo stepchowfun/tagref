62d69f06c1610bb2b0a0e2f08b79682c597cbb2a
Stephan Boyer <stephan@stephanboyer.com>
2020-10-23 15:25:20 -0700

    Install v1.3.3 by default

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@
   DESTINATION="${PREFIX:-/usr/local/bin}/tagref"
 
   # Which version to download
-  RELEASE="v${VERSION:-1.3.2}"
+  RELEASE="v${VERSION:-1.3.3}"
 
   # Determine which binary to download.
   FILENAME=''
