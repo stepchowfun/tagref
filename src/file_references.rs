b7b8ee761432fbfe70dbe7399aa3566a8bfcd858
Stephan Boyer <stephan@stephanboyer.com>
2026-04-12 16:05:37 -0700

    Release v1.12.0

--- src/file_references.rs
+++ src/file_references.rs
@@ -7,8 +7,11 @@ pub fn check(refs: &[Directive]) -> Vec<String> {
     let mut errors = Vec::<String>::new();
 
     for file in refs {
-        let file_path = resolve_target_path(&file.path, Path::new(&file.label));
-        match metadata(&file_path) {
+        // The `unwrap` is safe because `file.path` should always exist in some parent directory.
+        match metadata(resolve_target_path(
+            file.path.parent().unwrap(),
+            Path::new(&file.label),
+        )) {
             Ok(metadata) => {
                 if !metadata.is_file() {
                     errors.push(format!("{file} does not point to a file."));
